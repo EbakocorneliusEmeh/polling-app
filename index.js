8abd088a718361efb9a6ff3a2a1e8d39983accea
Ebakocornelius <ebakocornelius@gmail.com>
2025-09-22 12:50:34 +0100

    pushing work

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./src/routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
