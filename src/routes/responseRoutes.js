263e95706591985843eabbd673439fa9337083a4
Ebakocornelius <ebakocornelius@gmail.com>
2025-10-03 04:55:07 +0100

    working working

--- src/routes/responseRoutes.js
+++ src/routes/responseRoutes.js
@@ -1,16 +1,4 @@
 
-// // src/routes/responseRoutes.js
-// import express from "express";
-// import { submitResponses } from "../controllers/responseController.js";
-
-// const router = express.Router();
-
-// // POST /api/sessions/:sessionCode/responses
-// router.post("/:sessionCode/responses", submitResponses);
-
-// export default router;
-
-
 // src/routes/responseRoutes.js
 import express from "express";
 import { submitResponses, getSessionResponses } from "../controllers/responseController.js";
